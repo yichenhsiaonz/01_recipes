f04daa0f057559911aa258cdf2f84500713e9ca2
hsiaoy71950 <hsiaoy71950@masseyhigh.school.nz>
2020-02-05 10:52:11 +1300

    set up files

---
+++ component_1_user_input_string_checker.py
@@ -0,0 +1,10 @@
+def string_check(string, condition):
+    if len(string) <= 5:
+        if string in list <= 5:
+            valid = 0
+            return valid
+        else:
+            valid = 1
+            return valid
+    else:
+        
