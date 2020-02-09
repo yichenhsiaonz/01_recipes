20b41387c1bfc759ef4eb2622a55597620b7b778
hsiaoy71950 <hsiaoy71950@masseyhigh.school.nz>
2020-02-10 12:41:18 +1300

    Update component_1_user_input_string_checker.py

--- component_1_user_input_string_checker.py
+++ component_1_user_input_string_checker.py
@@ -1,10 +1,17 @@
 def string_check(string, condition):
+    global valid
     if len(string) <= 5:
-        if string in list <= 5:
+        if string in valid_list:
             valid = 0
             return valid
         else:
             valid = 1
             return valid
     else:
-        
+        valid = 0
+        return valid
+
+valid_list = ["sugar"]
+valid = 1
+while valid == 1:
+    string_check
