1607ba970cc93e0c06db82acc8dc693517c48635
hsiaoy71950 <hsiaoy71950@masseyhigh.school.nz>
2020-02-27 09:54:33 +1300

    Update component_7_split_inputs.py

--- component_7_split_inputs.py
+++ component_7_split_inputs.py
@@ -1,12 +1,18 @@
 import re
 
+print("please put a comma in between the amount, unit, and name")
+
 loop = ""
 while loop == "":
     try:
         user_input = input("Please enter an ingredient listed in the recipe in the order of amount, unit, then name")
+        if len(re.findall(",", user_input)) > 2:
+            raise ValueError
         ing_list = re.split(",", user_input, 3)
+        for x in ing_list:
+            if x == "":
+                raise ValueError
         amount = ing_list[0].replace(" ", "+")
-
         print("{} = {}".format(amount, eval(amount)))
         print(eval(amount), ing_list[1].strip(), ing_list[2].strip())
     except:
